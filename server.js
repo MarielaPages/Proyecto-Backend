e1b4de6c05ca034175c8f705a79f5d69d90cedc1
Glitch (hello-express) <none>
2022-04-28 02:57:54 +0000

    🎖🌝 Checkpoint ./server.js:44296972/1886

--- server.js
+++ server.js
@@ -3,9 +3,9 @@
 
 const express = require("express");
 const app = express();
-const Contenedor = require('./PagesMarielaDesafio4.js')
+const Contenedor = require("./PagesMarielaDesafio4.js");
 
-const archivoNuevo = new Contenedor('productos.txt');
+const archivoNuevo = new Contenedor("productos.txt");
 
 app.get("/productos", async (request, response) => {
   let products = await archivoNuevo.getAll();
@@ -14,12 +14,17 @@ app.get("/productos", async (request, response) => {
 
 app.get("/productoRandom", async (request, response) => {
   let products = await archivoNuevo.getAll();
-  let productRandomId = Math.floor(Math.random()*products.length)+1
-  let randomProduct = 
-  response.send(``);
+  let productRandomId = Math.floor(Math.random() * products.length) + 1;
+  let randomProduct = {};
+  products.forEach((product) => {
+    if (product.id === productRandomId) {
+      randomProduct = product;
+    }
+  });
+  response.send(`randomProduct`);
 });
 
-// listen for requests 
+// listen for requests
 const listener = app.listen(process.env.PORT, () => {
   console.log("Your app is listening on port " + listener.address().port);
 });
