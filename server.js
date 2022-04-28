a74b3ff56ca5245c80cc93c248d3ba328ba0bcc3
Glitch (hello-express) <none>
2022-04-28 03:33:07 +0000

    🤗🏉 Checkpoint ./server.js:44296972/2835

--- server.js
+++ server.js
@@ -3,34 +3,28 @@
 
 const express = require("express");
 const app = express();
-const Contenedor = require("./PagesMarielaDesafio4.js");
+const Contenedor = require('./PagesMarielaDesafio4.js')
 
-const archivoNuevo = new Contenedor("productos.txt");
-
-app.get("/", (request, response) => {
-  response.send(`Hola`);
-});
+const archivoNuevo = new Contenedor('productos.txt');
 
 app.get("/productos", async (request, response) => {
   let products = await archivoNuevo.getAll();
-  let arrayProducts = JSON.stringify(products);
-  response.send(`${arrayProducts}`);
+  response.send(`products`);
 });
 
 app.get("/productoRandom", async (request, response) => {
   let products = await archivoNuevo.getAll();
-  let productRandomId = Math.floor(Math.random() * products.length) + 1;
-  let randomProduct = {};
-  products.forEach((product) => {
-    if (product.id === productRandomId) {
+  let productRandomId = Math.floor(Math.random()*products.length)+1
+  let randomProduct = {}
+  product.forEach(product => {
+    if(product.id === productRandomId){
       randomProduct = product;
     }
-  });
-  response.send(`${randomProduct}`);
+  })
+  response.send(``);
 });
 
-// listen for requests
-const PORT = 8080;
-app.listen(PORT, () => {
-  console.log(`Your app is listening on port ${PORT}`);
+// listen for requests 
+const listener = app.listen(process.env.PORT, () => {
+  console.log("Your app is listening on port " + listener.address().port);
 });
